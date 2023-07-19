2077f8e5f3c380b1a1bad825f6d032d0f88014c1
jyekang <janicekangaroo@gmail.com>
2023-07-18 18:04:06 -0700

    venue and event urls working

--- tickit_project/tickit/urls.py
+++ tickit_project/tickit/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
-    path('venues/', views.venue_list, name='venue_list'),
-    path('venues/<int:pk>', views.venue_detail, name='venue_detail'),
-    path('events/', views.event_list, name='event_list'),
-    path('events/<int:pk>', views.event_detail, name='event_detail')
+    path('venues/', views.VenueList.as_view(), name='venue_list'),
+    path('venues/<int:pk>', views.VenueDetail.as_view(), name='venue_detail'),
+    path('events/', views.EventList.as_view(), name='event_list'),
+    path('events/<int:pk>', views.EventDetail.as_view(), name='event_detail')
 ]
