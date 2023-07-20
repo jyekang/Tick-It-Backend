2a52653af275a02c22083fa6f5f515daf706061e
ssstumbles <sara.rosic@gmail.com>
2023-07-20 12:31:23 -0400

    railway deployment attempt

--- tickit_project/tickit_django/settings.py
+++ tickit_project/tickit_django/settings.py
@@ -152,3 +152,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#hi
